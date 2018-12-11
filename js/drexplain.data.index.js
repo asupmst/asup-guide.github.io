f22f3397abaf26b2ca0ce79523c46064893d2ccc
ASUP <asup.mst@gmail.com>
2018-12-11 14:29:36 +0300

    asup-guide

---
+++ js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [2,1],
+	DREX_NODE_KEYWORDS_START: [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<НОВОЕ КЛЮЧ... Требуется Advanced-лицензия!","АС... Требуется Advanced-лицензия!","Руководство ... Требуется Advanced-лицензия!"],
+	DREX_KEYWORD_CHILD_START: [1,3,3],
+	DREX_KEYWORD_CHILD_END: [3,3,3]
+	
+};
