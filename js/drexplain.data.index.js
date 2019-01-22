c520f53bf64e44319b0aec9a4a30ed39cd851d7d
asupmst <asup.mst@gmail.com>
2019-01-22 10:56:55 +0300

    asupmt
    update

--- js/drexplain.data.index.js
+++ js/drexplain.data.index.js
@@ -2,12 +2,12 @@ DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
 DR_EXPLAIN.data_index = {
 
 	// index
-	DREX_NODE_KEYWORDS: [],
-	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
-	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS: [2,2,3,4,4,3],
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,1,4,6,6,6,6,6], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,1,4,6,6,6,6,6,6], //length:= drex.nodes_count,
 
-	DREX_KEYWORD_NAMES: ["<НОВОЕ КЛЮЧЕВОЕ СЛОВО>","Введите номер документа в ДО"],
-	DREX_KEYWORD_CHILD_START: [1,2],
-	DREX_KEYWORD_CHILD_END: [2,2]
+	DREX_KEYWORD_NAMES: ["<НОВОЕ КЛЮЧЕВОЕ СЛОВО>","Введите номер документа в ДО","заказ клиента","бюджет","статья бюджета"],
+	DREX_KEYWORD_CHILD_START: [1,5,5,5,5],
+	DREX_KEYWORD_CHILD_END: [5,5,5,5,5]
 	
 };
