a77f5c1197863f2fb230a3eb65644e5d437c0c0a
asupmst <asup.mst@gmail.com>
2019-01-22 18:13:48 +0300

    asupmt
    111

--- js/drexplain.data.index.js
+++ js/drexplain.data.index.js
@@ -2,9 +2,9 @@ DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
 DR_EXPLAIN.data_index = {
 
 	// index
-	DREX_NODE_KEYWORDS: [2,5,6,7,2,3,4,4,3,7,5,2,6,2,5,6,2,5,7,5,7,2,7,5,6,6],
-	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,4,7,9,9,12,13,13,13,16,19,22,24,25,25,26,26], //length:= drex.nodes_count,
-	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,4,7,9,9,12,13,13,13,16,19,22,24,25,25,26,26,26], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS: [2,5,6,7,7,5,2,3,4,4,3,7,5,2,6,2,5,6,2,5,7,5,7,2,7,5,6,6],
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,4,6,9,11,11,14,15,15,15,18,21,24,26,27,27,28,28], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,4,6,9,11,11,14,15,15,15,18,21,24,26,27,27,28,28,28], //length:= drex.nodes_count,
 
 	DREX_KEYWORD_NAMES: ["<НОВОЕ КЛЮЧЕВОЕ СЛОВО>","Введите номер документа в ДО","заказ клиента","бюджет","статья бюджета","сделка","сопровождение","разработка"],
 	DREX_KEYWORD_CHILD_START: [1,8,8,8,8,8,8,8],
